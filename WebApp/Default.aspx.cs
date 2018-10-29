32353faf68b5bf34c4965e06e7a248ff3a924cbb
Kierra0106 <37854862+Kierra0106@users.noreply.github.com>
2018-10-28 20:25:54 -0400

    Change

---
+++ WebApp/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var response = base.Response;
+            var context = base.Context;
+            //Write the timestamp to the page
+            response.Write(context.Timestamp);
+        }
+    }
+}
